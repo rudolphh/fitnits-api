68e67d72e3e27c58cf5c8488f796a39cfc3d3c46
Rudy A. Hernandez <rudolpharthur@gmail.com>
2022-01-22 00:59:39 -0800

    include id with _id in response

--- models/food.js
+++ models/food.js
@@ -1,47 +1,83 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const validator = require('validator');
+const validator = require("validator");
 
-let FoodSchema = new Schema({
-    calories: { type: Number, required: true, min: [ 0, 'Must be positive' ]},
+let FoodSchema = new Schema(
+  {
+    calories: { type: Number, required: true, min: [0, "Must be positive"] },
     name: {
-        type: String,
-        required: true,
-        lowercase: true
+      type: String,
+      required: true,
+      lowercase: true,
     },
-    protein: { type: Number, min: [ 0, 'Must be positive' ]},
-    carbohydrate: { type: Number, min: [ 0, 'Must be positive' ]},
-    sugars: { type: Number, min: [ 0, 'Must be positive' ]},
-    fat: { type: Number, min: [ 0, 'Must be positive' ]},
-    saturated: { type: Number, min: [ 0, 'Must be positive' ]},
-    unsaturated: { type: Number, min: [ 0, 'Must be positive' ]},
-    trans: { type: Number, min: [ 0, 'Must be positive' ]},
-    sodium: { type: Number, min: [ 0, 'Must be positive' ]},
+    protein: { type: Number, min: [0, "Must be positive"] },
+    carbohydrate: { type: Number, min: [0, "Must be positive"] },
+    sugars: { type: Number, min: [0, "Must be positive"] },
+    fat: { type: Number, min: [0, "Must be positive"] },
+    saturated: { type: Number, min: [0, "Must be positive"] },
+    unsaturated: { type: Number, min: [0, "Must be positive"] },
+    trans: { type: Number, min: [0, "Must be positive"] },
+    sodium: { type: Number, min: [0, "Must be positive"] },
     date: {
-        type: Date,
-        required: true,
-        default: Date.now,
-        validate: [ validator.isDate, "Date is invalid" ]
+      type: Date,
+      required: true,
+      default: Date.now,
+      validate: [validator.isDate, "Date is invalid"],
     },
     user: {
-        type: Schema.Types.ObjectId,
-        ref: 'user',
-        required: true
-     }
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    id: true,
+    toJSON: {
+      virtuals: true,
+      versionKey: true,
+    },
+  }
+);
 
-}, { timestamps: true });
+const Food = mongoose.model("food", FoodSchema, "foods");
 
-const Food = mongoose.model('food', FoodSchema, 'foods');
-
-const addFood = function(requestBody, user) {
-    const { calories, name, tags, protein, carbohydrate, sugars, fat, saturated,
-            unsaturated, trans, sodium, date } = requestBody
-    const food = new Food({ calories, name, tags, protein, carbohydrate, sugars, 
-                            fat, saturated, unsaturated, trans, sodium, date, user });
-    return food.save();
-}
+const addFood = function (requestBody, user) {
+  const {
+    id,
+    calories,
+    name,
+    tags,
+    protein,
+    carbohydrate,
+    sugars,
+    fat,
+    saturated,
+    unsaturated,
+    trans,
+    sodium,
+    date,
+  } = requestBody;
+  const food = new Food({
+    id,
+    calories,
+    name,
+    tags,
+    protein,
+    carbohydrate,
+    sugars,
+    fat,
+    saturated,
+    unsaturated,
+    trans,
+    sodium,
+    date,
+    user,
+  });
+  return food.save();
+};
 
 module.exports = {
-    Food,
-    addFood
-}
+  Food,
+  addFood,
+};
