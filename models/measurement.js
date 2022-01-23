ee92fb9aad8070cbfcccea5a77fd956057c88612
Rudy A. Hernandez <rudolpharthur@gmail.com>
2022-01-22 19:10:45 -0800

    measurement virtual id

--- models/measurement.js
+++ models/measurement.js
@@ -1,45 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const validator = require('validator');
+const validator = require("validator");
 
-let measurementSchema = new Schema({
-    weight: { type: Number, required: true, min: [0, 'Must be positive']},
-    neck: { type: Number, min: [0, 'Must be positive']},
-    waist: { type: Number, min: [0, 'Must be positive']},
-    hips: { type: Number, min: [0, 'Must be positive']},
+let measurementSchema = new Schema(
+  {
+    weight: { type: Number, required: true, min: [0, "Must be positive"] },
+    neck: { type: Number, min: [0, "Must be positive"] },
+    waist: { type: Number, min: [0, "Must be positive"] },
+    hips: { type: Number, min: [0, "Must be positive"] },
     unit: {
-        type: String,
-        required: true,
-        enum : ['imperial','metric'],
-        default: 'imperial'
+      type: String,
+      required: true,
+      enum: ["imperial", "metric"],
+      default: "imperial",
     },
     strategy: {
-        type: String,
-        required: true,
-        enum : [ 'cut','bulk' ],
+      type: String,
+      required: true,
+      enum: ["cut", "bulk"],
     },
     date: {
-        type: Date,
-        required: true,
-        default: Date.now,
-        validate: [ validator.isDate, "Date is invalid" ]
+      type: Date,
+      required: true,
+      default: Date.now,
+      validate: [validator.isDate, "Date is invalid"],
     },
     user: {
-        type: Schema.Types.ObjectId,
-        ref: 'user',
-        required: true
-     }
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    id: true,
+    toJSON: {
+      virtuals: true,
+      versionKey: true,
+    },
+  }
+);
 
-}, { timestamps: true });
+const Measurement = mongoose.model(
+  "measurement",
+  measurementSchema,
+  "measurements"
+);
 
-const Measurement = mongoose.model('measurement', measurementSchema, 'measurements');
-
-const addMeasurement = function(requestBody, user) {
-    const measurement = new Measurement({ ...requestBody, user });
-    return measurement.save();
-}
+const addMeasurement = function (requestBody, user) {
+  const measurement = new Measurement({ ...requestBody, user });
+  return measurement.save();
+};
 
 module.exports = {
-    Measurement,
-    addMeasurement
-}
+  Measurement,
+  addMeasurement,
+};
